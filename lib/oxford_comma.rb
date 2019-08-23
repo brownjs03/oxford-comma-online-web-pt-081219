103a254882c80146a8036a251eca2e5830bf5032
Jeffrey Brown <brownjs03@gmail.com>
2019-08-23 02:57:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,13 @@
 def oxford_comma(array)
   if array.size == 1 
     array.join
-elsif array.size == 2 
+  elsif array.size == 2 
     array.join(" and ")
-    elsif array.size >= 3
-    #array.join("," " and ")
-    word = array.pop
-    string = array.join(", ")
-    string << ", and #{word}"
-    #elsif array.size >= 4
-    #array.join("," " and ")
-    #phrase = array.pop
+    elsif array.size > 2
+    #word = array.pop
     #string = array.join(", ")
-    #string << ", and #{phrase}"
-    end
+    #string << ", and #{word}"
+    array[-1].insert(0, "and ")
+    array.join(", ")
+       end
 end
-
-#Jeff, you are looking into pulling out the last object of an array, storing it and then pushing back onto the end of the string and adding "and"
